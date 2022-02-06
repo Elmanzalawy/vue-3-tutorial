5a8a400f7cb78799d20821ddd361821b4d53dd3a
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2022-02-07 00:31:20 +0200

    Add `Modal` component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick()">Click Me</button>
+  <Modal/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 export default {
   name: 'App',
+  data(){
+    return { 
+      title:"My First Vue App",
+    }
+  },
+  methods: {
+    handleClick() {
+      // console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  },
   components: {
-    HelloWorld
+    Modal
   }
 }
 </script>
@@ -22,5 +36,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
 }
 </style>
