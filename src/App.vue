18808fa5c55fc8509aba021e1027e003f8fb7e85
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2022-02-10 00:01:20 +0200

    reaction-timer initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,40 @@
 <template>
-  <h1>{{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick()">Click Me</button>
-  <teleport to=".modals" v-if="showModal">
-    <Modal :header="modalHeader" :text="modalText" theme="sale" @closeModal="toggleModal">
-      <p>This is a slot component.</p>
-      <template v-slot:links>
-        <a href="#">Sign Up</a>
-        <a href="#">More Info</a>
-      </template>
-    </Modal>
-  </teleport>
-  <button @click="toggleModal">Open Modal</button>
-
-  <teleport to=".modals" v-if="showModalTwo">
-    <!--second modal challenge -->
-    <Modal header="modal header 2" text="modal text 2" theme="sale" @closeModal="toggleModalTwo">
-      <p>This is a slot component.</p>
-      <template v-slot:links>
-        <a href="#">Sign Up</a>
-        <a href="#">More Info</a>
-      </template>
-    </Modal>
-  </teleport>
-  <button @click="toggleModalTwo">Open Modal Two</button>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Modal from './components/Modal.vue'
+import Block from "./components/Block.vue"
 export default {
   name: 'App',
+  components: { Block },
   data(){
     return { 
-      title:"My First Vue App",
-      modalHeader: "Model Header",
-      modalText: "Modal Text",
-      showModal: false,
-      showModalTwo: false,
+      isPlaying: false,
+      delay: null,
     }
   },
   methods: {
-    handleClick() {
-      // console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus()
-    },
-    toggleModal(){
-      this.showModal = !this.showModal
-    },
-    toggleModalTwo(){
-      this.showModalTwo = !this.showModalTwo
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay)
     }
   },
-  components: {
-    Modal
-  }
+
 }
 </script>
 
 <style>
-#app, .modals {
+#app,
+.modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
-h1{
-  border-bottom: 1px solid #ddd;
-}
+
 </style>
