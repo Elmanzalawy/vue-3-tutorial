a161ded5f09219be9555f78e1df7a5e257f3781b
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2022-02-24 22:31:02 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,24 @@
 <template>
-  <h1>{{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick()">Click Me</button>
-  <teleport to=".modals" v-if="showModal">
-    <Modal :header="modalHeader" :text="modalText" theme="sale" @closeModal="toggleModal">
-      <p>This is a slot component.</p>
-      <template v-slot:links>
-        <a href="#">Sign Up</a>
-        <a href="#">More Info</a>
-      </template>
-    </Modal>
-  </teleport>
-  <button @click="toggleModal">Open Modal</button>
-
-  <teleport to=".modals" v-if="showModalTwo">
-    <!--second modal challenge -->
-    <Modal header="modal header 2" text="modal text 2" theme="sale" @closeModal="toggleModalTwo">
-      <p>This is a slot component.</p>
-      <template v-slot:links>
-        <a href="#">Sign Up</a>
-        <a href="#">More Info</a>
-      </template>
-    </Modal>
-  </teleport>
-  <button @click="toggleModalTwo">Open Modal Two</button>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Modal from './components/Modal.vue'
 export default {
   name: 'App',
   data(){
     return { 
-      title:"My First Vue App",
-      modalHeader: "Model Header",
-      modalText: "Modal Text",
-      showModal: false,
-      showModalTwo: false,
+
     }
   },
   methods: {
-    handleClick() {
-      // console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus()
-    },
-    toggleModal(){
-      this.showModal = !this.showModal
-    },
-    toggleModalTwo(){
-      this.showModalTwo = !this.showModalTwo
-    }
+
   },
   components: {
-    Modal
   }
 }
 </script>
 
 <style>
-#app, .modals {
+#app, {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
